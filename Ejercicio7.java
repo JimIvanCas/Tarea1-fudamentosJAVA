5e1015ca5b63f0e9dd55531d9115ded32b508c85
Ivan Dario Jimenez Casas <148558659+JimIvanCas@users.noreply.github.com>
2023-11-05 12:04:22 -0500

    Update Ejercicio7.java

--- Ejercicio7.java
+++ Ejercicio7.java
@@ -1,13 +1,15 @@
 import java.util.Scanner;
 public class Ejercicio7 {
     public static void main(String[] args){
+// Declarar la variable tipo Scanner. 
         Scanner scanner = new Scanner(System.in);
-
+// Solicitar y declarar la variable 1. 
         System.out.print("Ingresa el radio de la Circunferencia: ");
         double Radio = scanner.nextDouble();
+// Realizar el proceso logico. 
         double Longitud = 2*3.14159*Radio;
         double Area = 3.14159*Radio*Radio;
-
+// Resultado. 
         System.out.println("La longitud de la Circunferencia es: "+Longitud);
         System.out.print("El Área del Circulo es: "+Area);
     }
