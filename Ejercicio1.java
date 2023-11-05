65ce0397080c07696f44b90ea93afe6af124418b
Ivan Dario Jimenez Casas <148558659+JimIvanCas@users.noreply.github.com>
2023-11-05 11:53:24 -0500

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 public class Ejercicio1 {
     public static void main(String[] args) {
+// Declarar la variable Scanner.
         Scanner scanner = new Scanner(System.in);
-
+// Solicitar y declarar la variable 1.
         System.out.print("Ingresa la base del triangulo: ");
         double base = scanner.nextDouble();
-
+// Solicitar y declarar la variable 2. 
         System.out.print("Ingresa la altura del triangulo: ");
         double altura = scanner.nextDouble();
-
+// Realizar el proceso logico. 
         double area = (base * altura)/2;
-
+// Resultado. 
         System.out.print("El area del triangulo es: "+ area);
     }
 }
