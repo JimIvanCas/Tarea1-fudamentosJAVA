4eee457099063c9756fb0ad1ee9abaedee211ef6
Ivan Dario Jimenez Casas <148558659+JimIvanCas@users.noreply.github.com>
2023-11-05 11:57:34 -0500

    Update Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,13 +1,14 @@
 import java.util.Scanner;
 public class Ejercicio3 {
     public static void main(String[] args){
+//Declarar la variable Scanner. 
         Scanner scanner = new Scanner(System.in);
-
+// Solicitar y declarar la variable 1. 
         System.out.print("Ingresa el número: ");
         double numero = scanner.nextDouble();
-
+// Realizar el proceso logico. 
         double resultado = (numero * numero);
-
+// Resultado. 
         System.out.print("El cuadrado de "+numero+ " "+"es: "+resultado);
     }
 }
