2d11d888ecac54fcf3682681bcafd6b7611df1de
Ivan Dario Jimenez Casas <148558659+JimIvanCas@users.noreply.github.com>
2023-11-05 12:06:14 -0500

    Update Ejercicio8.java

--- Ejercicio8.java
+++ Ejercicio8.java
@@ -1,17 +1,20 @@
 import java.util.Scanner;
 public class Ejercicio8 {
     public static void main(String[] args){
+// Declarar la variable de tipo Scanner. 
         Scanner impresion = new Scanner(System.in);
-
+// Solicitar la variable 1. 
         System.out.print("Ingresa el primer número: ");
         double Primernumero = impresion.nextDouble();
+// Solicitar la variable 2.
         System.out.print("Ingrese el segundo número: ");
         double Segundonumero = impresion.nextDouble();
+// Solicitar la variable 3. 
         System.out.print("Ingrese el tercer número: ");
         double Tecernumero = impresion.nextDouble();
-
+// Realizar el proceso logico. 
         double Promedio = (Primernumero+Segundonumero+Tecernumero)/3;
+// Resultado. 
         System.out.print("El promedio de los numeros indicados es: "+Promedio);
-
     }
 }
