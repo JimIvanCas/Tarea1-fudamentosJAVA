902e046ae5ec6f571fc76583b9ce58aed2a88504
Ivan Dario Jimenez Casas <148558659+JimIvanCas@users.noreply.github.com>
2023-11-05 11:56:10 -0500

    Update Ejercicio2.java

--- Ejercicio2.java
+++ Ejercicio2.java
@@ -1,16 +1,17 @@
 import java.util.Scanner;
 public class Ejercicio2 {
     public static void main(String[] args){
+//Declarar la variable Scanner.
         Scanner scanner = new Scanner(System.in);
-
+// Solicitar y declarar la variable 1.
         System.out.print("Ingresa el primer número: ");
         int numero1 = scanner.nextInt();
-
+// Solicitar y declarar la variable 2. 
         System.out.print("Ingresa el segundo número: ");
         int numero2 = scanner.nextInt();
-
+// Realizar el proceso logico. 
         int suma = (numero1 + numero2);
-
+// Resultado. 
         System.out.print("La suma de los números es: "+ suma);
     }
 }
